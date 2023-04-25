3c1fbb0dff917502dfcc9ae91955db943e48e011
Miroslav Vanek <46657262+Miroslav1234@users.noreply.github.com>
2023-04-25 11:02:50 +0200

    Update app.py
    Signed-off-by: Miroslav Vanek <46657262+Miroslav1234@users.noreply.github.com>

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Cats")
-option_b = os.getenv('OPTION_B', "Dogs")
+option_a = os.getenv('OPTION_A', "Václav")
+option_b = os.getenv('OPTION_B', "Pavel")
 hostname = socket.gethostname()
 version = 'v1'
 
